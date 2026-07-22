3088e6379aecf2f891a0e41b2e928dc90140ec91
Anfisa Kovganyuk <anfisa.kovganyuk@gmail.com>
2026-07-22 22:56:24 +0400

    refactor(audio): expose shared signal loader

--- src/voice_match/services/__init__.py
+++ src/voice_match/services/__init__.py
@@ -1,5 +1,6 @@
 """Сервисный слой voice_match."""
 
+from voice_match.services.audio import load_audio_signal
 from voice_match.services.comparison import compare_voices_dual
 
-__all__ = ['compare_voices_dual']
+__all__ = ['compare_voices_dual', 'load_audio_signal']
